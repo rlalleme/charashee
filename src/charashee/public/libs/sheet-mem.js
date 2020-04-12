d602a677536161cbb9427c80d4b6d8840afefa5f
Raphaël Lallement <raphael.lallement@scalian.com>
2020-04-12 11:59:22 +0200

    [non-atomic] change the way the data are stored/retrieved, store the name of the form above the content, detect the form only when they are of class 'toSave'

--- src/charashee/public/libs/sheet-mem.js
+++ src/charashee/public/libs/sheet-mem.js
@@ -1,12 +1,64 @@
 const maxLength = 65000
 
-// Save form state to hashtag
-function storeSheet(client, uuid) {
+function buildJSONContent() {
 	//Grab all the form data and put it into an array.
 	var result = {};
-	$.each($('form#charashee').serializeArray(), function() {
-		result[this.name] = this.value;
+	$.each($("form.toSave :input").not(".noSave"), function() {
+		var name=this.getAttribute('name'); //Reject elements that do not have a 'name'
+		if(name == undefined || name == ''){
+			return;
+		}
+		
+		if(this.value == undefined || this.value == ''){ //Reject elements that have an empty field (saves space)
+			return;
+		}
+		
+		//First retrieve the closest form with a name
+		var parent="";
+		var curr=this.parentNode;
+		while(curr!=undefined){
+			if(curr.id != undefined && curr.id != "" && curr.tagName == "FORM"){
+				parent=curr.id;
+			}
+			curr=curr.parentNode;
+		}
+		
+		//Then prepare the value
+		var value;
+		switch(this.type||this[0].type){
+			default:
+				value=this.value;
+				break;
+			case"radio":
+			case"checkbox":
+// TODO FIX IF NECESSARY	if(this.length)
+// 					for(var l=0;l<this.length;l++)
+// 						/*r.indexOf(c[l].value)>-1*/=this[l].checked;
+// 				else 
+					value=this.checked;/*=r.indexOf(c.value)>-1;*/
+				break;
+		}
+		
+		//Finally verify that the result structure already has the correct parent (if not null)
+		//And store the value
+		if(parent!=""){
+			if(result[parent]==undefined){
+				result[parent]={};
+			}
+			result[parent][name]=value;
+		}else{
+			result[name]=value;
+		}
 	});
+	return result;
+}
+
+// Save form state to hashtag
+// If a client is provided, send an MQTT message. (Players should provide their client, the MJ should not (infinite loop))
+function storeSheet(client, uuid) {
+	//Grab all the form data and put it into an array.
+	var result = buildJSONContent();
+	
 	// Alert to show what's being saved for debugging purposes. Should be commented out in live
 	// alert(JSON.stringify(result));
 	// Convert array to JSON string which is then compressed to a text string that can be stored in the URI
@@ -37,10 +89,16 @@ function retrieveSheet() {
 	var charData = LZString.decompressFromEncodedURIComponent(charHashtag);
 	// Parse JSON data
 	var data = JSON.parse(charData);
-	// Set your containing element
-	var formElement = document.getElementById('charashee');
-	// populate the form with our JSON object
-	populate(formElement, data);
+	// Browse all containing elements (should all be forms)
+	for(var property in data){
+		if(data.hasOwnProperty(property)){
+			var index=property,value=data[property];
+			if("object"==typeof value){
+				var formElement = document.getElementById(index);
+				populate(formElement, value);
+			}
+		}
+	}
 }
 
 function exportFile(filename) {
@@ -48,8 +106,6 @@ function exportFile(filename) {
 	var charData = LZString.decompressFromEncodedURIComponent(charHashtag);
 	var json = JSON.stringify(JSON.parse(charData), null, 2)
 
-	console.log(json);
-	
 	var element = document.createElement('a');
 	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(json));
 	element.setAttribute('download', filename+'.json');
